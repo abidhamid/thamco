a019ec3b48d8fb39f9bad3368141ad1b070801fa
ABID HAMID <N3212892@live.tees.ac.uk>
2018-01-03 17:36:30 +0000

    included test files

---
+++ ThAmCo.Tests/Controllers/TestControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThAmCo;
+using ThAmCo.Controllers;
+
+namespace ThAmCo.Tests.Controllers
+{
+    [TestClass]
+    public class TestControllerTest
+    {
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            // Arrange
+            TestController controller = new TestController();
+
+            // Act
+            ContentResult result = controller.Test1() as ContentResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
+
+    }
+}
